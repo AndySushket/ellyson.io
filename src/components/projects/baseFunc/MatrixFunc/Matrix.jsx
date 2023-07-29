802d77c3659ceecc9ff8b292fe846c3a8f7991f5
Andrii Sushket <a.sushket@svitla.com>
2023-07-29 11:57:20 +0500

    refactor config

--- src/components/projects/baseFunc/MatrixFunc/Matrix.jsx
+++ src/components/projects/baseFunc/MatrixFunc/Matrix.jsx
@@ -2,56 +2,53 @@
  * Created by Ellyson on 5/11/2018.
  */
 
-import TemplateFor3D from '../../../templates/mainTemplate3D';
-import * as THREE from 'three';
-import * as dat from 'dat.gui';
+import * as THREE from "three";
+import * as dat from "dat.gui";
+import TemplateFor3D from "../../../templates/mainTemplate3D";
 
 export default class Matrix extends TemplateFor3D {
+  componentWillUnmount() {
+    super.componentWillUnmount();
+    this.gui.destroy();
+  }
 
-	componentWillUnmount() {
-		super.componentWillUnmount();
-		this.gui.destroy();
-	}
+  initControls() {
+    super.initControls();
+    this.camera.position.set(0, 0, 10);
+  }
 
-	initControls() {
-		super.initControls();
-		this.camera.position.set(0, 0, 10);
-	}
+  initPositionRotationScale() {
+    const geometry = new THREE.BoxGeometry(5, 5, 5);
+    const material = new THREE.MeshNormalMaterial();
+    this.cube = new THREE.Mesh(geometry, material);
+    this.scene.add(this.cube);
 
-	initPositionRotationScale() {
-		const geometry = new THREE.BoxGeometry(5, 5, 5);
-		const material = new THREE.MeshNormalMaterial();
-		this.cube = new THREE.Mesh(geometry, material);
-		this.scene.add(this.cube);
+    this.gui = new dat.GUI();
+    const positionFolder = this.gui.addFolder("position");
 
-		this.gui = new dat.GUI();
-		const positionFolder = this.gui.addFolder('position');
+    this.Matrix = this.cube.matrix.clone();
+    this.cube.matrixAutoUpdate = false;
+    this.Matrix.elements.forEach((elem, i) => {
+      positionFolder.add(this.Matrix.elements, i, -5, 5, 0.1);
+    });
+    positionFolder.add(this, "applyMatrix");
 
-		this.Matrix = this.cube.matrix.clone();
-		this.cube.matrixAutoUpdate = false;
-		this.Matrix.elements.forEach((elem, i) => {
-			positionFolder.add(this.Matrix.elements, i, -5,5, 0.1);
-		});
-		positionFolder.add(this, "applyMatrix");
+    positionFolder.open();
+  }
 
-		positionFolder.open();
-	}
+  componentDidMount() {
+    this.init3D();
+    this.initPositionRotationScale();
+    this.initControls();
+    this.animate();
+  }
 
-	componentDidMount() {
-		this.init3D();
-		this.initPositionRotationScale();
-		this.initControls();
-		this.animate();
-	}
+  applyMatrix() {
+    this.cube.applyMatrix(this.Matrix);
+  }
 
-	applyMatrix(){
-		this.cube.applyMatrix(this.Matrix);
-	}
-
-	animate(){
-		if (!this.looped) return;
-		super.animate();
-	}
-
-
+  animate() {
+    if (!this.looped) return;
+    super.animate();
+  }
 }
