495b4bb735ef93b9869f7c2d315acf8120be7995
Andrii Sushket <a.sushket@svitla.com>
2023-07-29 12:46:20 +0500

    refactor config

--- config-overrides.js
+++ config-overrides.js
@@ -1,14 +1,18 @@
 const path = require("path");
-const { addWebpackModuleRule, override, addWebpackAlias} = require("customize-cra");
+const {
+  addWebpackModuleRule,
+  override,
+  addWebpackAlias,
+} = require("customize-cra");
 
 module.exports = {
   webpack: override(
     addWebpackAlias({
       "@": path.resolve(__dirname, "src"),
-      components: path.resolve(__dirname, 'src/components'),
-      assets: path.resolve(__dirname, 'src/assets'),
-      utils: path.resolve(__dirname, 'src/utils'),
-      styles: path.resolve(__dirname, 'src/styles'),
+      components: path.resolve(__dirname, "src/components"),
+      assets: path.resolve(__dirname, "src/assets"),
+      utils: path.resolve(__dirname, "src/utils"),
+      styles: path.resolve(__dirname, "src/styles"),
     }),
     addWebpackModuleRule({
       test: /\.(shader|vert|frag|glsl|fnt)$/,
