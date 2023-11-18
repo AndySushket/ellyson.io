9fec58fe036305a5df3be213ce5deaa8fde69d6b
Andrii Sushket <a.sushket@svitla.com>
2023-11-18 16:24:39 +0100

    ar test

--- src/components/projects/vasilika/Dance/index.jsx
+++ src/components/projects/vasilika/Dance/index.jsx
@@ -7,9 +7,7 @@ import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
 import {LinearProgress} from "@mui/material";
 import React from "react";
 import TemplateFor3D from "components/templates/mainTemplate3D";
-import model from "./bellydancing.fbx";
-import model2 from "./model3.fbx";
-import model3 from "./Bellydancing (1).fbx";
+import danceModel from "./danceModel.fbx";
 
 export default class Index extends TemplateFor3D {
 
@@ -32,18 +30,12 @@ export default class Index extends TemplateFor3D {
   }
 
   onSelect() {
-
-      // document.addEventListener('touchstart', {handleEvent: (event) => {
-      //     // alert();
-      // }});
-
     if (!this.isLoaded) {
       const loader = new FBXLoader();
-
-      [model, model2, model3].forEach((fbx, i) => {
+      [danceModel].forEach((fbx, i) => {
           loader.load(fbx, (object) => {
 
-              // object.scale.setScalar(0.000001); // very big model
+              object.scale.setScalar(0.000001); // very big model
 
               const aabb = new THREE.Box3();
               aabb.setFromObject(object);
@@ -57,13 +49,11 @@ export default class Index extends TemplateFor3D {
 
               const action = mixer.clipAction(object.animations[0]);
               action.play();
-
-              console.log(this.mixers);
               this.scene.add(object);
           }, (xhr) => {
               this.setState({loadProcess: xhr.loaded / xhr.total * 100});
           }, (error) => {
-              console.log(error, i);
+              console.log(error);
           });
       })
       this.isLoaded = true;
@@ -74,7 +64,7 @@ export default class Index extends TemplateFor3D {
     this.init3D({ antialias: true, alpha: true }, undefined, {ar: true});
     this.initLight();
     this.buttonAr?.addEventListener( 'click', () => {
-      console.log("AR MODE");
+        //action on touch
     });
     this.onSelect()
     this.initControls();
@@ -98,11 +88,9 @@ export default class Index extends TemplateFor3D {
     }
 
     const delta = this.clock.getDelta();
-    // if (this.mixer) this.mixer.update( this.clock.getDelta() * 1.5 );
-      this.mixers.forEach((mixer) => {
-
-          mixer.update(delta);
-      });
+    this.mixers.forEach((mixer) => {
+        mixer.update(delta);
+    });
     this.renderer?.render(this.scene, this.camera);
   }
 
@@ -111,7 +99,6 @@ export default class Index extends TemplateFor3D {
     const { loadProcess } = this.state;
     return (
         <div>
-          {/* <header /> */}
           {loadProcess < 100 && <LinearProgress variant="determinate" value={loadProcess} />}
           <div ref={ (ref)=> { this.canvasDiv = ref}} className="canvasDiv" />
         </div>
