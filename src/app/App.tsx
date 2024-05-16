4f74c280f4eea8f5ecb59cb72b38d57ba380ee3d
Andrii Sushket <a.sushket@svitla.com>
2024-05-16 19:43:29 +0200

    firebase analytics

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import '../styles/App.scss';
 import 'react-bootstrap';
 import {BrowserRouter} from "react-router-dom";
@@ -7,10 +7,17 @@ import {createTheme} from "@mui/material";
 import Header from "./Header";
 import Routing from './Routing';
 
+import { analytics, logEvent } from "../firebase/firebaseConfig";
+
 const theme = createTheme({
 });
 
 export default function App() {
+
+    useEffect(() => {
+        logEvent(analytics,'page_view');
+    }, []);
+
     const url = window.location.pathname;
     const hideHeader = url.includes('vasilika') || url === '/';
         return (
