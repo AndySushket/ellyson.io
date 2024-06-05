23dbe8dfc7dcfed6d6e7ebe2d7a3ecb4c1584394
Andrii Sushket <a.sushket@svitla.com>
2024-06-05 18:31:57 +0200

    projects in progress

--- src/app/Main/pages/Projects.tsx
+++ src/app/Main/pages/Projects.tsx
@@ -17,148 +17,154 @@ function Projects({location}: {location: string}) {
     else setExit(false);
   }, [location]);
 
+  const projectsList = <div>
+    <Col>
+      <Row>
+        <div>
+          <Button>
+            <Link to="/mainFunc/PositionRotationScale">
+              Position Rotation Scale
+            </Link>
+          </Button>
+          <Button>
+            <Link to="/mainFunc/Quaternion">Quaternion</Link>
+          </Button>
+          <Button>
+            <Link to="/mainFunc/Matrix">Matrix</Link>
+          </Button>
+        </div>
+      </Row>
+      <Row>
+        <div>
+          <Button>
+            <Link to="/Shaders/Shader1">Shader1</Link>
+          </Button>
+          <Button>
+            <Link to="/Shaders/Shader2">Shader2</Link>
+          </Button>
+          <Button>
+            <Link to="/Shaders/Shader3">Shader3</Link>
+          </Button>
+          <Button>
+            <Link to="/Shaders/Shader4">Shader4</Link>
+          </Button>
+        </div>
+      </Row>
+      <Row>
+        <div>
+          <Button>
+            <Link to="/musicVisualization/MusicVisualization">
+              MusicVisualization
+            </Link>
+          </Button>
+        </div>
+      </Row>
+      <Row>
+        <div>
+          <Button>
+            <Link to="/Space/Galaxy">Galaxy</Link>
+          </Button>
+          <Button>
+            <Link to="/Space/Earth">Earth</Link>
+          </Button>
+          <Button>
+            <Link to="/Space/Planet">Planet</Link>
+          </Button>
+        </div>
+      </Row>
+      <Row>
+        <div>
+          <Button>
+            <Link to="/remakeTutorialWorks/thanosPortal">
+              Thanos Portal
+            </Link>
+          </Button>
+          <Button>
+            <Link to="/remakeTutorialWorks/rain">Rain</Link>
+          </Button>
+        </div>
+      </Row>
+      <Row>
+        <div>
+          <Button>
+            <Link to="/akella/Pepyaka">Pepyaka</Link>
+          </Button>
+          <Button>
+            <Link to="/akella/DisplacedBox">Displaced Box</Link>
+          </Button>
+          <Button>
+            <Link to="/akella/TriangleWallpaper">
+              TriangleWallpaper
+            </Link>
+          </Button>
+          <Button>
+            <Link to="/akella/CreepingStripes">Creeping stripes</Link>
+          </Button>
+        </div>
+      </Row>
+      <Row>
+        <div>
+          <Button>
+            <Link to="/SomeWorks/Aviator">Aviator</Link>
+          </Button>
+          <Button>
+            <Link to="/SomeWorks/PointLights">PointLights</Link>
+          </Button>
+        </div>
+      </Row>
+      <Row>
+        <div>
+          <Button>
+            <Link to="/OtherPeoplesWork/Portal">Portal</Link>
+          </Button>
+          <Button>
+            <Link to="/OtherPeoplesWork/Benares">Benares</Link>
+          </Button>
+        </div>
+      </Row>
+      <Row>
+        <div>
+          <Button>
+            <Link to="/testWork/House">House</Link>
+          </Button>
+        </div>
+      </Row>
+      <Row>
+        <div>
+          <Button>
+            <Link to="/ThreeJsJourney/Texture">Texture</Link>
+          </Button>
+        </div>
+      </Row>
+    </Col>
+  </div>
+
+  const projectsList2 = <h1>
+    In progress
+  </h1>
+
   return (
-    <AnimatePresence>
-      {!exit && (
-        <motion.div
-          className="project-list"
-          initial={{ opacity: 0, backdropFilter: "blur(0)" }}
-          animate={{ opacity: 1, backdropFilter: "blur(20px)" }}
-          exit={{ opacity: 0, backdropFilter: "blur(0)" }}
-          transition={{ duration: .75 }}
-        >
-          <div>
-            <Col>
-              <Row>
-                <div>
-                  <Button>
-                    <Link to="/mainFunc/PositionRotationScale">
-                      Position Rotation Scale
-                    </Link>
-                  </Button>
-                  <Button>
-                    <Link to="/mainFunc/Quaternion">Quaternion</Link>
-                  </Button>
-                  <Button>
-                    <Link to="/mainFunc/Matrix">Matrix</Link>
-                  </Button>
-                </div>
-              </Row>
-              <Row>
-                <div>
-                  <Button>
-                    <Link to="/Shaders/Shader1">Shader1</Link>
-                  </Button>
-                  <Button>
-                    <Link to="/Shaders/Shader2">Shader2</Link>
-                  </Button>
-                  <Button>
-                    <Link to="/Shaders/Shader3">Shader3</Link>
-                  </Button>
-                  <Button>
-                    <Link to="/Shaders/Shader4">Shader4</Link>
-                  </Button>
-                </div>
-              </Row>
-              <Row>
-                <div>
-                  <Button>
-                    <Link to="/musicVisualization/MusicVisualization">
-                      MusicVisualization
-                    </Link>
-                  </Button>
-                </div>
-              </Row>
-              <Row>
-                <div>
-                  <Button>
-                    <Link to="/Space/Galaxy">Galaxy</Link>
-                  </Button>
-                  <Button>
-                    <Link to="/Space/Earth">Earth</Link>
-                  </Button>
-                  <Button>
-                    <Link to="/Space/Planet">Planet</Link>
-                  </Button>
-                </div>
-              </Row>
-              <Row>
-                <div>
-                  <Button>
-                    <Link to="/remakeTutorialWorks/thanosPortal">
-                      Thanos Portal
-                    </Link>
-                  </Button>
-                  <Button>
-                    <Link to="/remakeTutorialWorks/rain">Rain</Link>
-                  </Button>
-                </div>
-              </Row>
-              <Row>
-                <div>
-                  <Button>
-                    <Link to="/akella/Pepyaka">Pepyaka</Link>
-                  </Button>
-                  <Button>
-                    <Link to="/akella/DisplacedBox">Displaced Box</Link>
-                  </Button>
-                  <Button>
-                    <Link to="/akella/TriangleWallpaper">
-                      TriangleWallpaper
-                    </Link>
-                  </Button>
-                  <Button>
-                    <Link to="/akella/CreepingStripes">Creeping stripes</Link>
-                  </Button>
-                </div>
-              </Row>
-              <Row>
-                <div>
-                  <Button>
-                    <Link to="/SomeWorks/Aviator">Aviator</Link>
-                  </Button>
-                  <Button>
-                    <Link to="/SomeWorks/PointLights">PointLights</Link>
-                  </Button>
-                </div>
-              </Row>
-              <Row>
-                <div>
-                  <Button>
-                    <Link to="/OtherPeoplesWork/Portal">Portal</Link>
-                  </Button>
-                  <Button>
-                    <Link to="/OtherPeoplesWork/Benares">Benares</Link>
-                  </Button>
-                </div>
-              </Row>
-              <Row>
-                <div>
-                  <Button>
-                    <Link to="/testWork/House">House</Link>
-                  </Button>
-                </div>
-              </Row>
-              <Row>
-                <div>
-                  <Button>
-                    <Link to="/ThreeJsJourney/Texture">Texture</Link>
-                  </Button>
-                </div>
-              </Row>
-            </Col>
-          </div>
-        </motion.div>
-      )}
-    </AnimatePresence>
+      <AnimatePresence>
+        {!exit && (
+            <motion.div
+                className="project-list"
+                initial={{opacity: 0, backdropFilter: "blur(0)"}}
+                animate={{opacity: 1, backdropFilter: "blur(20px)"}}
+                exit={{opacity: 0, backdropFilter: "blur(0)"}}
+                transition={{duration: .75}}
+            >
+                {projectsList2}
+            </motion.div>
+        )}
+      </AnimatePresence>
   );
 }
 
 function mapStateToProps(state: { ui: any }) {
   const {
-    ui: { location },
+    ui: {location},
   } = state;
-  return { location };
+  return {location};
 }
 
 export default connect(mapStateToProps)(Projects);
