4696bba2ed3cdf7beadb15dd94504c68b3a200b5
Andrii Sushket <a.sushket@svitla.com>
2024-10-31 00:20:57 +0700

    -moving to next.js

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,15 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  webpack: (config, { isServer }) => {
+    config.module.rules.push(
+      {
+        test: /\.(shader|vert|frag|glsl|fnt)$/,
+        use: 'raw-loader',
+      },
+    );
+    return config;
+  },
   output: 'standalone', // Outputs a Single-Page Application (SPA).
   distDir: './build', // Changes the build output directory to `./dist`.
 }
