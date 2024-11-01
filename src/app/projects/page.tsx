8603de51257f8a021f1ac8542a922f996672096e
Andrii Sushket <a.sushket@svitla.com>
2024-11-01 22:13:13 +0700

    -moving to next.js

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,18 +4,15 @@
 
 import React from 'react';
 
-// function ProjectList() {
-//
-//   const projectsList2 = <h1>In progress</h1>;
-//
-//   return <div
-//     className="project-list"
-//     style={{ opacity: 1, backdropFilter: 'blur(20px)' }}>
-//     {projectsList2}
-//   </div>;
-// }
-//
-// export default ProjectList;
-export default function ProjectsPage() {
-  return <div>Projects Page</div>;
+function ProjectList() {
+
+  const projectsList2 = <h1>In progress</h1>;
+
+  return <div
+    className="project-list"
+    style={{ opacity: 1, backdropFilter: 'blur(20px)' }}>
+    {projectsList2}
+  </div>;
 }
+
+export default ProjectList;
