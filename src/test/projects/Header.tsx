69fd73e5883010edbb2d731bacda610933757a4b
Andrii Sushket <a.sushket@svitla.com>
2024-11-01 23:04:27 +0700

    -moving to next.js

--- src/test/projects/Header.tsx
+++ src/test/projects/Header.tsx
@@ -1,5 +1,6 @@
-import React from "react";
-import Button from "@mui/material/Button";
+import React from 'react';
+import Button from '@mui/material/Button';
+import Link from 'next/link';
 
 export default function Header() {
   return (
@@ -7,11 +8,15 @@ export default function Header() {
       <div className="leftHeader">Andy Sushket</div>
       <div className="rightHeader">
         <div>
-          <Button className="btn btn-primary-outline" href="/">Home</Button>
+          <Button className="btn btn-primary-outline">
+            <Link href="/">Home</Link>
+          </Button>
         </div>
         /
         <div>
-          <Button className="btn btn-primary-outline" href="/projects">Projects</Button>
+          <Button className="btn btn-primary-outline">
+            <Link href="/projects">Projects</Link>
+          </Button>
         </div>
       </div>
     </div>
